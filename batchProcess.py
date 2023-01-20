0d69c39a17ed6678b679a9509bf05b7080e5d8a1
Vipul Kumar <vipulvk.kumar@gmail.com>
2023-01-20 13:01:52 +0200

    handling batch size and final result array of arrays

--- batchProcess.py
+++ batchProcess.py
@@ -3,32 +3,57 @@ import sys
 from pympler import asizeof
 
 def split_array(arr, chunk_size):
+    """Splits the given array arr into batches of size defined by chunk_size"""
     chunks = []
     for i in range(0,len(arr),chunk_size):
         chunks.append(arr[i:i+chunk_size])
     return chunks
 
 def worker(chunk):
+    """worker method to filter the larger records and further divide the batch if chunk size is more than the limit"""
     #processing
-    print(chunk)
+    result = []
     for s in chunk:
-        print("String: " + s + "Size: " + str(asizeof.asizeof(s)))
-        if sys.getsizeof(s) > max_record_size:
+        if asizeof.asizeof(s) > max_record_size:
             chunk.remove(s)
-    print(chunk)
-    print("---------------")
-    return chunk
+    #print(chunk)
+    if asizeof.asizeof(chunk) > max_batch_size:
+        """if batch size is more than the minit, furhter processing it to smaller batches starting with half the length"""
+        chunks=split_array(chunk, (len(chunk)//2)+1)
+        with ThreadPoolExecutor(max_workers=max_thread_pool) as executor:
+            results = list(executor.map(worker,chunks))
+            #print(results)
+            result.extend(results)
+    else:
+        result.extend(chunk)
+    return result
+
+def open_nested_list(list_obj):
+    """Converting a nested list object to single list of only single list objects"""
+    res = []
+    for ele in list_obj:
+        if isinstance(ele, list):
+            res.extend(open_nested_list(ele))
+        else:
+            res.append(list_obj)
+            break
+    return res
 
 
-def main():
-    arr = ['qwerty','pofgvffiuyt','zxsw','qw','qwe','mnbvc','mnbvc','lkjh','lkijuh','plmnbvcx','lokij','lokijn','lkj','po','lkjn','poiu','ag','lo','ytg','qw','poi']
+
+def batch_process(arr):
     chunk_size = 3
     chunks=split_array(arr, chunk_size)
-    with ThreadPoolExecutor(max_workers=4) as executor:
+    res = []
+    with ThreadPoolExecutor(max_workers=max_thread_pool) as executor:
         results = list(executor.map(worker,chunks))
-        print(results)
+        res = open_nested_list(results)
+        print(res)
 
 max_record_size = 56 #change it to 1 MB equivalent of bytes
-max_batch_size = 170 #change it to 5 MB equivalent of bytes
+max_batch_size = 220 #change it to 5 MB equivalent of bytes
+max_thread_pool = 4 #max number of threads
 
-main()
+arr = ['qwerty','pofgvffiuyt','zxsw','qw','qwe','mnbvc','mnbvc','lkjh','lkijuh','plmnbvcx','lokij','lokijn','lkj','po','lkjn','poiu','ag','lo','ytg','qw','poi']
+
+batch_process(arr)
